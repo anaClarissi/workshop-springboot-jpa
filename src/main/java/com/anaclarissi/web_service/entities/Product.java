ab1c436327d0d4be2c6194ea45cc832da1a972dd
Ana Clarissi <anaclarissicruz@gmail.com>
2026-02-28 17:40:02 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/anaclarissi/web_service/entities/Product.java
+++ src/main/java/com/anaclarissi/web_service/entities/Product.java
@@ -1,5 +1,6 @@
 package com.anaclarissi.web_service.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -23,6 +24,9 @@ public class Product implements Serializable {
     @ManyToMany
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product () {}
 
@@ -103,6 +107,21 @@ public class Product implements Serializable {
     public Set<Category> getCategories() {
 
         return categories;
+
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+
+        Set<Order> set = new HashSet<>();
+
+        for (OrderItem orderItem : this.items) {
+
+            set.add(orderItem.getOrder());
+
+        }
+
+        return set;
 
     }
 
