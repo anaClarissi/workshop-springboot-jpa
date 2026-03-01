3be42a6ef5ff2430a4e2e0086525e16a38ab7d55
Ana Clarissi <anaclarissicruz@gmail.com>
2026-03-01 18:43:48 -0300

    User delete

--- src/main/java/com/anaclarissi/web_service/resources/UserResource.java
+++ src/main/java/com/anaclarissi/web_service/resources/UserResource.java
@@ -46,4 +46,13 @@ public class UserResource {
 
     }
 
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
+
+    }
+
 }
